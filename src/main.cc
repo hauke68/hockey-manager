d810bda543d10ff16907c809c6d220597bbbd975
Hauke Zuehl <hzuehl@phone-talk.de>
2012-07-04 10:18:32 +0200

    Making main.cc compilable with g++

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,35 @@
-#include "main.h"
+#include <ClanLib/application.h>
+#include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include <ClanLib/gl.h>
+#include <ClanLib/gui.h>
+
+#include <vector>
+
+using namespace std;
+
+class Main {
+
+public:
 
 // Implementation of class Main
 #ifdef WINDOWS
-static int Main::WinMain(const vector<CL_String> &args) {
+	static int WinMain(const vector<CL_String> &args) {
 #else
-static int Main::main(const vector<CL_String> &args) {
+	static int main(const vector<CL_String> &args) {
 #endif
 
-	// Setup ClanLib modules
-	CL_SetupCore    setup_core;
-	CL_SetupDisplay setup_display;
-	CL_SetupGL      setup_gl;
-	CL_SetupGUI     setup_gui;
+		// Setup ClanLib modules
+		CL_SetupCore    setup_core;
+		CL_SetupDisplay setup_display;
+		CL_SetupGL      setup_gl;
+		CL_SetupGUI     setup_gui;
 
-	// Start the game here
+		// Start the game here
 
-	return 0;
-}
+		return 0;
+	}
+};
 
 #ifdef WINDOWS
 CL_ClanApplication app(&Main::WinMain);
